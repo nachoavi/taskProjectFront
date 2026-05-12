0d66b74697fed7f72be6376f10f4d37aef83e50b
Luis San Martin <luis17.sanmartin@gmail.com>
2026-05-12 17:20:59 -0400

    minors bugs fixed

--- src/components/demo/DemoAdminTasks.jsx
+++ src/components/demo/DemoAdminTasks.jsx
@@ -278,24 +278,6 @@ export default function DemoAdminTasks() {
             </table>
           </div>
         )}
-                        <button
-                          onClick={() => handleDelete(task.id)}
-                          className="btn btn-danger btn-sm"
-                          title="Eliminar tarea"
-                        >
-                          <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-                            <polyline points="3,6 5,6 21,6"/>
-                            <path d="M19 6v14a2 2 0 01-2 2H7a2 2 0 01-2-2V6m3 0V4a2 2 0 012-2h4a2 2 0 012 2v2"/>
-                          </svg>
-                        </button>
-                      </div>
-                    </td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
-        )}
       </div>
     </div>
   );
